a22b42e28b4858380a69ae9b71cf0fe7f8d0c758
vitali-droujko <vitali.droujko@epitech.eu>
2021-01-26 16:53:14 +0100

    adds the possibility of restarting another calculation

--- calculatrice.js
+++ calculatrice.js
@@ -8,18 +8,7 @@
 
 var mode;
 var a0, a1;
-
-while (mode != 1 && mode != 2 && mode != 3 && mode != 4) {
-    mode = Number(prompt("Choisissez un mode de calcul (de 1 à 4) ?\n\n1 - Addition\n2 - Soustraction\n3 - Multiplication\n4 - Division\n\n"));
-}
-
-while (isNaN(a0)) {
-    a0 = Number(prompt('premier nombre :\n'));
-}
-
-while (isNaN(a1)) {
-    a1 = Number(prompt('second nombre :\n'));
-}
+var again;
 
 function addition(a0, a1) {
     return (a0 + a1);
@@ -39,25 +28,45 @@ function division(a0, a1) {
     return (a0 / a1);
 }
 
-try {
-    switch(parseInt(mode)) {
-        case 1:
-            var result = addition(a0, a1);
-            break;
-        case 2:
-            var result = soustraction(a0, a1);
-            break;
-        case 3:
-            var result = multiplication(a0, a1);
-            break;
-        case 4:
-            var result = division(a0, a1);
-            break;
-        default:
-            throw new Error("Une erreur est survenue");
+again = false;
+
+do {
+    do {
+        mode = Number(prompt("Choisissez un mode de calcul (de 1 à 4) ?\n\n1 - Addition\n2 - Soustraction\n3 - Multiplication\n4 - Division\n\n"));
+    } while (mode != 1 && mode != 2 && mode != 3 && mode != 4) 
+    
+    do {
+        a0 = Number(prompt('premier nombre :\n'));
+    } while (isNaN(a0))
+    
+    do {
+        a1 = Number(prompt('second nombre :\n'));
+    } while (isNaN(a1))
+    
+    
+    try {
+        switch(parseInt(mode)) {
+            case 1:
+                var result = addition(a0, a1);
+                break;
+            case 2:
+                var result = soustraction(a0, a1);
+                break;
+            case 3:
+                var result = multiplication(a0, a1);
+                break;
+            case 4:
+                var result = division(a0, a1);
+                break;
+            default:
+                throw new Error("Une erreur est survenue");
+        }
+        alert("voici le resultat : " + result);
     }
-    alert("voici le resultat : " + result);
-}
-catch(error) {
-    alert(error);
-}
+    catch(error) {
+        alert(error);
+    }
+
+    again = confirm('Voulez vous faire un autre calcul ?');
+
+} while (again)
